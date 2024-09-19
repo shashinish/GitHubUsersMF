4d487171f73df369670e6e12abb8985d9a1efb1b
Shashi <shashinishantha@gmail.com>
2024-09-19 23:18:55 +0800

    Initial commit

---
+++ GitHubUsersMF/Configs/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GitHubUsersMF
+//
+//  Created by Shashi Nishantha on 9/19/24.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API{
+        struct 
+    }
+}
---
+++ GitHubUsersMF/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitHubUsersMF
+//
+//  Created by Shashi Nishantha on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
